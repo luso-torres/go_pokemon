621543d9fca551e301058a8f89fdcbba40495b78
Luso <63476039+luso-torres@users.noreply.github.com>
2025-01-11 17:26:23 -0300

    new function sprintf

---
+++ investment_calculator2.0/investment_calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount, expectedReturnRate, years float64
+	const inflationRate = 6.5
+
+	fmt.Print("Insert the invesment amount:")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Insert the Expected Return Rate:")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Insert how many years you expect to invest:")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow((1.0+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+
+	formattedFV := fmt.Sprintf("Expected value is %.2f\n", futureValue)
+	formattedFRV := fmt.Sprintf("The future value (adjusted by inflation) is %.2f\n", futureRealValue)
+	//fmt.Printf("Expected value is %.2f\n", futureValue)
+	//fmt.Printf("The future value (adjusted by inflation) is %.2f\n", futureRealValue)
+	print(formattedFV, formattedFRV)
+
+}
