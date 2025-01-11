0f798f112e36378cc8744ba7c41604e22e85e85d
Luso <63476039+luso-torres@users.noreply.github.com>
2025-01-11 16:57:05 -0300

    added comments

--- practise-1-go/profit_calculator.go
+++ practise-1-go/profit_calculator.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+/* Created by Luso
+Inputs:
+revenue => int/float type representing $
+expenses => int/float type represeting $
+taxRate => int/float in percentage values, e.g. 24 representing 24%
+-------------------------------------------------------------------------------------------
+Outputs:
+earningsBeforeTaxes => float type represeting $
+earningsAfterTaxes => float type represeting $
+ratio => in percentage values, e.g. 40 representing 40%.
+-------------------------------------------------------------------------------------------
+Assumptions:
+	The earnings before taxes were simply obtained by subtracting the variables:
+		earningsBeforeTaxes = revenue - expenses
+	It was assumed that the tax is discounted based on your revenues,
+	not in your earnings before taxes as follows:
+		earningsAfterTaxes = revenue(1-taxRate/100) - expenses
+	Finally, to evaluate the ratio, it was simply the division of the two previous parameters:
+		ratio = earningsBeforeTaxes/earningsAfterTaxes
+*/
+
 func main() {
 	var revenue, expenses, taxRate, ratio, earningsBeforeTax, earningsAfterTax float64
 
