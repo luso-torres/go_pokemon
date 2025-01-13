481cce61854915ee81f95fac4e311ae8a40760cf
Luso <63476039+luso-torres@users.noreply.github.com>
2025-01-12 21:28:04 -0300

    added new function in investment 3.0

--- building_functions/investment_calculator3.0/investment_calculator.go
+++ building_functions/investment_calculator3.0/investment_calculator.go
@@ -5,9 +5,10 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
 	var investmentAmount, expectedReturnRate, years float64
-	const inflationRate = 6.5
 
 	fmt.Print("Insert the invesment amount:")
 	fmt.Scan(&investmentAmount)
@@ -16,19 +17,24 @@ func main() {
 	fmt.Print("Insert how many years you expect to invest:")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1.0+expectedReturnRate/100), years)
-	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Expected value is %.2f\n", futureValue)
 	formattedFRV := fmt.Sprintf("The future value (adjusted by inflation) is %.2f\n", futureRealValue)
 	//fmt.Printf("Expected value is %.2f\n", futureValue)
 	//fmt.Printf("The future value (adjusted by inflation) is %.2f\n", futureRealValue)
 	print(formattedFV, formattedFRV)
-	/* If you want to output a longer text on your print, instead of "" characters, utilize ``
-	in this case \n is treated as a string instead of an external command. It can be useful in future
-	e.g.
-	fmt.Printf(`Expected value is %.2f\n
-		* any text here*
-	The future value (adjusted by inflation) is %.2f\n`, futureValue, futureRealValue)
-	*/
+}
+
+/*func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	FV := investmentAmount * math.Pow(1.0+expectedReturnRate/100, years)
+	FRV := FV / math.Pow(1+inflationRate/100, years)
+	return FV, FRV
+}*/
+//we could do instead
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (FV float64, FRV float64) {
+	FV = investmentAmount * math.Pow(1.0+expectedReturnRate/100, years)
+	FRV = FV / math.Pow(1+inflationRate/100, years)
+	return
 }
