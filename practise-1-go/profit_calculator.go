7f9a433c1e4ffbd31d466e4e9770510fc542e341
Luso <63476039+luso-torres@users.noreply.github.com>
2025-01-12 21:46:21 -0300

    changes to the original practise

--- practise-1-go/profit_calculator.go
+++ practise-1-go/profit_calculator.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-/* Created by Luso
+/* Created by Luso - Version 1.1
 Inputs:
 revenue => int/float type representing $
 expenses => int/float type represeting $
@@ -21,24 +21,38 @@ Assumptions:
 	It was assumed that the tax is discounted based on your revenues,
 	not in your earnings before taxes as follows:
 		earningsAfterTaxes = revenue(1-taxRate/100) - expenses
+	** corrected to the formula:
+		earningsAfterTaxes = EBT(1-taxRate/100)
 	Finally, to evaluate the ratio, it was simply the division of the two previous parameters:
-		ratio = earningsBeforeTaxes/earningsAfterTaxes
+		ratio = EBT/profit
 */
 
 func main() {
-	var revenue, expenses, taxRate, ratio, earningsBeforeTax, earningsAfterTax float64
+	var revenue, expenses, taxRate, earningsBeforeTax, earningsAfterTax, ratio float64
+	revenue, expenses, taxRate = requestValues()
 
-	fmt.Print("Insert your revenue: ")
+	earningsBeforeTax, earningsAfterTax, ratio = calculateProfit(revenue, expenses, taxRate)
+
+	showValues(earningsBeforeTax, earningsAfterTax, ratio)
+}
+
+func requestValues() (revenue float64, taxRate float64, expenses float64) {
+	fmt.Print("Revenue: ")
 	fmt.Scan(&revenue)
-	fmt.Print("Insert the taxes rate paid: ")
+	fmt.Print("Tax rate: ")
 	fmt.Scan(&taxRate)
-	fmt.Print("Insert your expenses: ")
+	fmt.Print("Expenses: ")
 	fmt.Scan(&expenses)
+	return revenue, taxRate, expenses
+}
 
-	earningsBeforeTax = revenue - expenses
-	fmt.Println("Your earnings before tax are: ", earningsBeforeTax)
-	earningsAfterTax = revenue*(1-taxRate/100) - expenses
-	fmt.Println("Your earnings after tax are: ", earningsAfterTax)
-	ratio = earningsBeforeTax / earningsAfterTax
-	fmt.Print("The ratio is: ", ratio)
+func calculateProfit(revenue float64, taxRate float64, expenses float64) (EBT float64, profit float64, ratio float64) {
+	EBT = revenue - expenses
+	profit = revenue*(1-taxRate/100) - expenses
+	ratio = EBT / profit
+	return EBT, profit, ratio
+}
+
+func showValues(EBT, profit, ratio float64) {
+	fmt.Printf("Your earnings before tax are: %.2f \nYour earnings after tax are: %.2f \nThe ratio is: %.2f", EBT, profit, ratio)
 }
